bb566fea1e1dbcd5f35a565ebf49b4bda1134b2b
Marcus Poehls <marcus.poehls@gmail.com>
2016-05-26 08:26:23 +0200

    add JSON response example

---
+++ examples/reply-json/base-route.js
@@ -0,0 +1,30 @@
+var baseRoutes = {
+  register: function (server, options, next) {
+    var routes = [
+      {
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+          var data = {
+            key: 'value',
+            another: false,
+            number: 10
+          }
+
+          reply(data)
+        }
+      }
+    ]
+
+    // add defined routes to hapi
+    server.route(routes)
+    next()
+  }
+}
+
+baseRoutes.register.attributes = {
+  name: 'base-routes',
+  version: '1.0.0'
+}
+
+module.exports = baseRoutes
