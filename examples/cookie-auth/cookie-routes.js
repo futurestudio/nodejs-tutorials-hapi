2c83654cba27aa8f2a21a6945f1a1bb9143d2050
Marcus Poehls <marcus.poehls@gmail.com>
2016-06-14 10:37:11 +0200

    implement hapi cookie auth example

---
+++ examples/cookie-auth/cookie-routes.js
@@ -0,0 +1,80 @@
+var Boom = require('boom')
+var Bcrypt = require('bcrypt')
+var Users = require('./users-db')
+
+var routes = [
+  {
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: {
+        mode: 'try',
+        strategy: 'session'
+      },
+      plugins: {
+        'hapi-auth-cookie': {
+          redirectTo: false
+        }
+      },
+      handler: function (request, reply) {
+        if (request.auth.isAuthenticated) {
+          return reply.view('profile')
+        }
+
+        reply.view('index')
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/',
+    config: {
+      // auth: 'session',
+      auth: {
+        mode: 'try'
+      },
+      plugins: {
+        'hapi-auth-cookie': {
+          redirectTo: false
+        }
+      },
+      handler: function (request, reply) {
+        if (request.auth.isAuthenticated) {
+          return reply.view('Profile')
+        }
+
+        var username = request.payload.username
+        var user = Users[ username ]
+
+        if (!user) {
+          return reply(Boom.notFound('No user registered with given credentials'))
+        }
+
+        var password = request.payload.password
+
+        return Bcrypt.compare(password, user.password, function (err, isValid) {
+          if (isValid) {
+            request.server.log('info', 'user authentication successful')
+            request.cookieAuth.set(user);
+            return reply.view('profile')
+          }
+
+          return reply.view('index')
+        })
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/logout',
+    config: {
+      auth: 'session',
+      handler: function (request, reply) {
+        request.cookieAuth.clear();
+        reply.view('index')
+      }
+    }
+  }
+]
+
+module.exports = routes
