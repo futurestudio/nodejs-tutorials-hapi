1f866071266e72159b502d0f4ee2d4403dd09d3f
Marcus Poehls <marcus.poehls@gmail.com>
2016-06-22 09:47:43 +0200

    add boom dependency; add example code for response status codes

---
+++ examples/response-status-codes/base-route.js
@@ -0,0 +1,50 @@
+var Boom = require('boom')
+
+var baseRoutes = {
+  register: function (server, options, next) {
+    var routes = [
+      {
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+          var data = {
+            key: 'value'
+          }
+
+          reply(data).code(201)
+        }
+      },
+      {
+        method: 'GET',
+        path: '/error',
+        handler: function (request, reply) {
+          var error = Boom.notFound('Cannot find the requested page')
+          reply(error)
+        }
+      },
+      {
+        method: 'GET',
+        path: '/empty',
+  config: {
+    response: {
+      emptyStatusCode: '204'
+    },
+    handler: function (request, reply) {
+      reply()
+    }
+  }
+      }
+    ]
+
+    // add defined routes to hapi
+    server.route(routes)
+    next()
+  }
+}
+
+baseRoutes.register.attributes = {
+  name: 'response-status',
+  version: '1.0.0'
+}
+
+module.exports = baseRoutes
