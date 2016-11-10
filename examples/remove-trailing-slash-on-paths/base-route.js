07cee84d7de61962c113b9e8883ea4f3073c199d
Marcus Poehls <marcus.poehls@gmail.com>
2016-11-10 11:33:37 +0100

    add example with option stripTrailingSlash: true

---
+++ examples/remove-trailing-slash-on-paths/base-route.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const baseRoutes = {
+  register: function (server, options, next) {
+    // add defined route to hapi server
+    server.route([
+      {
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+          reply('Did the work :)')
+        }
+      },
+      {
+        method: 'GET',
+        path: '/slash',
+        handler: function (request, reply) {
+          reply('Called route with slash at the end')
+        }
+      },
+      {
+        method: 'GET',
+        path: '/slash/{name}',
+        handler: function (request, reply) {
+          reply('Called route with slash at the end: ' + request.params.name)
+        }
+      }
+     ])
+
+    next()
+  }
+}
+
+baseRoutes.register.attributes = {
+  name: 'base-routes',
+  version: '1.0.0'
+}
+
+module.exports = baseRoutes
