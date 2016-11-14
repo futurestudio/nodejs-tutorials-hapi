b9096202556e3e70c8dec19543e512966fd3526c
Marcus Poehls <marcus.poehls@gmail.com>
2016-11-14 08:43:40 +0100

    fix reference error

--- examples/log-errors-to-sentry/error-handler.js
+++ examples/log-errors-to-sentry/error-handler.js
@@ -1,9 +1,9 @@
 'use stric'
 
-const client = server.plugins[ 'hapi-raven' ].client
-
 const ErrorManager = {
   reportError: function (server, error) {
+    const client = server.plugins[ 'hapi-raven' ].client
+
     client.captureException(error, function (result) {
       server.log('info', 'logged error in sentry with id: ' + result.id)
     })
