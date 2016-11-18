12b31bbb08c874b400814cbb2361458b11cadc94
Marcus Poehls <marcus.poehls@gmail.com>
2016-11-18 15:42:45 +0100

    add example for query parameter validation

---
+++ examples/validation-query-parameter/base-route.js
@@ -0,0 +1,38 @@
+var Joi = require('joi')
+
+var plugin = {
+  register: function (server, options, next) {
+    var routes = [
+      {
+        method: 'GET',
+        path: '/tutorials',
+        config: {
+          handler: function (request, reply) {
+            var queryParams = request.query
+
+            server.log('info', queryParams)
+
+            reply(queryParams)
+          },
+          validate: {
+            query: {
+              filter: Joi.array().items(Joi.string().valid('premium', 'video')).single()
+            }
+          }
+        }
+      }
+    ]
+
+    // add defined routes to hapi
+    server.route(routes)
+
+    next()
+  }
+}
+
+plugin.register.attributes = {
+  name: 'validate-query-parameters',
+  version: '1.0.0'
+}
+
+module.exports = plugin
