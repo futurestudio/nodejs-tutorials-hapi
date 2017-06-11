7c806de7ac69b6778415173fe39a6f3529784b2b
Marcus Poehls <marcus.poehls@gmail.com>
2017-06-11 13:17:41 +0200

    add example for custom 404 handler that replies a view

---
+++ examples/404-route-and-handler/base/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const Routes = require('./routes')
+
+exports.register = (server, options, next) => {
+  server.dependency([ 'vision' ])
+
+  server.route(Routes)
+  server.log('info', 'Plugin registered: 404 handler')
+
+  next()
+}
+
+exports.register.attributes = {
+  name: '404-handler',
+  version: '1.0.0'
+}
