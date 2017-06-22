81a828be724f92e14f73d3fb67ebe8945eeebdb2
Marcus Poehls <marcus.poehls@gmail.com>
2017-06-22 10:03:47 +0200

    404 handler: send JSON for REST requests

--- examples/404-route-and-handler/base/handler.js
+++ examples/404-route-and-handler/base/handler.js
@@ -1,15 +1,25 @@
 'use strict'
 
+const Boom = require('boom')
+
 const Handler = {
   index: {
-    handler: function (request, reply) {
+    handler: (request, reply) => {
       reply.view('index')
     }
   },
 
   missing: {
-    handler: function (request, reply) {
-      reply.view('404').code(404)
+    handler: (request, reply) => {
+      const accept = request.raw.req.headers.accept
+      const code = 404
+
+      // take priority: check header if there’s a JSON REST request
+      if (accept && accept.match(/json/)) {
+        return reply(Boom.notFound('Fuckity fuck, this resource isn’t available.'))
+      }
+
+      reply.view('404').code(code)
     }
   }
 }
